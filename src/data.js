3857eb9a0873f2ad1092373553e0cf3cfa69948b
Gokul S <87302750+Gokul-28@users.noreply.github.com>
2023-09-10 17:00:31 +0530

    navbar js created

---
+++ src/data.js
@@ -0,0 +1,19 @@
+// import image1 from './images/Kerala.jpg'
+// import image2 from './images/Animalpark.jpg'
+// import image3 from './images/Tajmahal.jpg'
+// import image4 from './images/Ladakh.jpg'
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+
